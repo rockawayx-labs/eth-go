5c0110fcad205605c87a8d940932ee6511937522
Stéphane Duchesneau <stephane.duchesneau@streamingfast.io>
2024-03-11 17:09:30 -0400

    fix type of blobGasUsed and excessBlobGas in rpc type header

--- rpc/types.go
+++ rpc/types.go
@@ -350,8 +350,8 @@ type Block struct {
 	UnclesSHA3       eth.Hash           `json:"sha3Uncles,omitempty"`
 	Uncles           []eth.Hash         `json:"uncles,omitempty"`
 
-	BlobGasUsed           *eth.Uint256 `json:"blobGasUsed,omitempty"`           // EIP-4844
-	ExcessBlobGas         *eth.Uint256 `json:"excessBlobGas,omitempty"`         // EIP-4844
+	BlobGasUsed           *eth.Uint64  `json:"blobGasUsed,omitempty"`           // EIP-4844
+	ExcessBlobGas         *eth.Uint64  `json:"excessBlobGas,omitempty"`         // EIP-4844
 	ParentBeaconBlockRoot *eth.Hash    `json:"parentBeaconBlockRoot,omitempty"` // EIP-4844
 	WithdrawalsHash       *eth.Hash    `json:"withdrawalsHash,omitempty"`       // EIP-4895
 	Withdrawals           []Withdrawal `json:"withdrawals,omitempty"`           // EIP-4895
